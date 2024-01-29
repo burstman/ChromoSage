41f67f25d281c143382c5a597cf7fc6af7b6c6e4
burstman <hamedflissi@gmail.com>
2024-01-29 17:32:55 +0100

    upload update version

--- data_gatharing.py
+++ data_gatharing.py
@@ -3,15 +3,15 @@ from sqlalchemy import text
 
 
 # Function to insert data into PostgreSQL
-def insert_into_postgres(insert_color, id_colori, couleur, id_type_tissu, numero_de_bain, delta_a, delta_b, delta_h, delta_l, delta_e, target, percent_red, percent_blue, percent_yellow):
+def insert_into_postgres(insert_color, id_colori, couleur, id_type_tissu, numero_de_bain, delta_a, delta_b, delta_h, delta_l, delta_e, decisions, percent_red, percent_blue, percent_yellow, percent_black):
 
     conn = st.connection("gather_db", type="sql")
 
     # Insert data into main_table
     with conn.session as session:
         sql_expression = text("""
-            INSERT INTO gathering (id_colori, couleur, id_type_de_tissu, numero_de_bain, delta_a, delta_b, delta_h, delta_l, delta_e, target)
-            VALUES (:id_colori, :couleur, :id_type_de_tissu, :numero_de_bain, :delta_a, :delta_b, :delta_h, :delta_l, :delta_e, :target)
+            INSERT INTO gathering (id_colori, couleur, id_type_de_tissu, numero_de_bain, delta_a, delta_b, delta_h, delta_l, delta_e, decisions)
+            VALUES (:id_colori, :couleur, :id_type_de_tissu, :numero_de_bain, :delta_a, :delta_b, :delta_h, :delta_l, :delta_e, :decisions)
         """)
         session.execute(sql_expression, {
             'id_colori': id_colori,
@@ -23,7 +23,7 @@ def insert_into_postgres(insert_color, id_colori, couleur, id_type_tissu, numero
             'delta_h': delta_h,
             'delta_l': delta_l,
             'delta_e': delta_e,
-            'target': target,
+            'decisions': decisions,
         })
         session.commit()
     if insert_color:
@@ -32,14 +32,15 @@ def insert_into_postgres(insert_color, id_colori, couleur, id_type_tissu, numero
                 'SELECT data_gather_id FROM gathering ORDER BY data_gather_id DESC LIMIT 1'))
             gather_id = result.scalar()
             sql_expression = text("""
-                                INSERT INTO color_correction (data_gather_id,percent_red,percent_blue,percent_yellow)
-                                VALUES (:data_gather_id, :percent_red, :percent_blue, :percent_yellow)
+                                INSERT INTO color_correction (data_gather_id,percent_red,percent_blue, percent_yellow, percent_black)
+                                VALUES (:data_gather_id, :percent_red, :percent_blue, :percent_yellow,:percent_black)
                                 """)
             session.execute(sql_expression, {
                 'data_gather_id': gather_id,
                 'percent_red': percent_red,
                 'percent_blue': percent_blue,
                 'percent_yellow': percent_yellow,
+                'percent_black':percent_black,
             })
             session.commit()
 
@@ -51,6 +52,7 @@ def main():
     percent_red = 0
     percent_blue = 0
     percent_yellow = 0
+    percent_black = 0
     # Create a container for the main inputs
     main_container = st.container(border=True,)
 
@@ -70,40 +72,40 @@ def main():
         col4, col5, col6 = st.columns(3)
 
         with col4:
-            numero_de_bain = st.number_input(
-                "Numero_de_Bain", min_value=0, step=1)
+            numero_de_bain = st.text_input("Numero_de_Bain")
 
         with col5:
-            delta_a = st.number_input("Delta(a)", min_value=0.0, step=0.1)
+            delta_a = st.number_input("Delta(a)", step=0.1)
 
         with col6:
-            delta_b = st.number_input("Delta(b)", min_value=0.0, step=0.1)
+            delta_b = st.number_input("Delta(b)", value=0.0,step=0.1)
 
         col7, col8, col9 = st.columns(3)
 
         with col7:
-            delta_h = st.number_input("Delta(h)", min_value=0.0, step=0.1)
+            delta_h = st.number_input("Delta(h)", step=0.1)
 
         with col8:
-            delta_l = st.number_input("Delta(L)", min_value=0.0, step=0.1)
+            delta_l = st.number_input("Delta(L)",  step=0.1)
 
         with col9:
-            delta_e = st.number_input("Delta(E)", min_value=0.0, step=0.1)
+            delta_e = st.number_input("Delta(E)",  step=0.1)
 
         # Dropdown for selecting the targets
-        targets = [
+        decisions = [
             "passer_sans_aucune_correction",
             "passer_avec_savonnage_fort",
             "passer_avec_savonnage_faible",
             "passer_avec_savonnage_faibe sans neutralisation",
             "passer_avec_savonnage_fort sans neutralisation",
-            "Ne pas passer faire_correction",
-            "passer au savonnage et blocker"
+            "ne_pas_passer_et_faire_correction",
+            "passer_au_savonnage_et_faire_correction_dans_un_nouveau_bain",
+            "passer_au_savonnage_et_blocker"
         ]
-        selected_target = st.selectbox("Select Target", targets)
+        selected_target = st.selectbox("Select Decision", decisions)
         insertcolor = False
         # If the selected target is "color_correction", provide sliders for color percentages
-        if selected_target == "Ne pas passer faire_correction":
+        if selected_target in ("ne_pas_passer_et_faire_correction", "passer_au_savonnage_et_faire_correction_dans_un_nouveau_bain"):
             insertcolor = True
             col10, col11, col12 = st.columns(3)
 
@@ -112,21 +114,22 @@ def main():
 
             with col11:
                 percent_blue = st.slider("Percentage of Blue (%)", 0, 100, 0)
+                percent_black = st.slider("Percentage of black (%)", 0, 100, 0)
 
             with col12:
                 percent_yellow = st.slider(
                     "Percentage of Yellow (%)", 0, 100, 0)
 
-            target_value = f"{selected_target} ({percent_blue}% Blue, {percent_red}% Red, {percent_yellow}% Yellow)"
+            decision_value = f"{selected_target} ({percent_blue}% Blue, {percent_red}% Red, {percent_yellow}% Yellow,{percent_black}% Black)"
         else:
-            target_value = selected_target
+            decision_value = selected_target
 
     # Button to gather and save data
     if st.button("Submit"):
         try:
             # Insert data into PostgreSQL
             insert_into_postgres(insertcolor, id_colori, couleur, id_type_tissu, numero_de_bain, delta_a,
-                                 delta_b, delta_h, delta_l, delta_e, target_value, percent_red, percent_blue, percent_yellow)
+                                 delta_b, delta_h, delta_l, delta_e, decision_value, percent_red, percent_blue, percent_yellow, percent_black)
 
             st.success("Data saved successfully.")
         except Exception as e:
